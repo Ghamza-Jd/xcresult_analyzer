aaad63ea092458a3ff995521756c4a21d07c38dd
Ghamza-Jd <hamzah.jadeed@gmail.com>
2024-10-18 18:12:01 +0300

    feat: added the ability to use builtin template
    Co-authored-by: mchehab34 <mac34@mail.aub.edu>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 mod dba;
+#[macro_use]
+mod macros;
 
 use anyhow::Result;
 use clap::Parser;
@@ -13,12 +15,23 @@ struct Cli {
     /// Path to the output file
     #[clap(short = 'o', long = "output")]
     output_path: PathBuf,
+    #[cfg(not(feature = "built_in"))]
     /// Path to the Handlebars template file
     #[clap(short = 't', long = "template")]
     template_path: PathBuf,
     /// Path to the xcresult
     #[clap(short = 'p', long = "xcresult")]
     xcresult_path: PathBuf,
+
+    #[cfg(feature = "built_in")]
+    /// Built-in template
+    #[clap(short = 'b', long = "built-in")]
+    built_in_template: BuiltInTemplate,
+}
+
+#[derive(Clone, clap::ValueEnum)]
+enum BuiltInTemplate {
+    Markdown,
 }
 
 fn main() -> Result<()> {
@@ -28,8 +41,13 @@ fn main() -> Result<()> {
 
     let test_results = dba.get_test_results()?;
     let params: serde_json::Value = serde_json::to_value(&test_results)?;
+    #[cfg(not(feature = "built_in"))]
     let content = fs::read_to_string(args.template_path)?;
-    let content = reg.render_template(content.as_str(), &params)?;
+    #[cfg(feature = "built_in")]
+    let content = match args.built_in_template {
+        BuiltInTemplate::Markdown => include_str!("../templates/markdown.hbs").to_string(),
+    };
+    let content = reg.render_template(&content, &params)?;
 
     fs::write(args.output_path, content)?;
     Ok(())
